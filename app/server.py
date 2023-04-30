8dd59ad6f35b6f4a0c1b8734c4d97b0535ffaa22
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2023-04-29 21:37:25 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1e8-tAZL0vZfi1tiO9n1f7yy0riC1qOdQ?alt=media&key=AIzaSyAW2D0JtFnb1lS_cbcf-MPJcu0TzbucrJ0'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1e8-tAZL0vZfi1tiO9n1f7yy0riC1qOdQ'
 export_file_name = 'model.pkl'
 
 classes = ['cat', 'dog']
